b7b722bde76a144430cfd57c31f3ed17aa4e2362
argunidem <argun.idem@hotmail.com>
2022-10-28 04:27:01 +0200

    second commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 document.querySelector('.hamburger-btn').addEventListener('click', () => {
   document.querySelector('.hamburger-btn').style.transform =
     'translateY(-200px)';
-  // document.querySelector('.nav-menu').style.transform = 'translateX(0)';
   document.querySelector('.nav-menu').style.opacity = '1';
 });
 
 document.querySelector('.nav-menu').addEventListener('click', () => {
   document.querySelector('.hamburger-btn').style.transform = 'translateY(0)';
-  // document.querySelector('.nav-menu').style.transform = 'translateX(100%)';
   document.querySelector('.nav-menu').style.opacity = '0';
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 1024) {
+    document.querySelector('.nav-menu').style.opacity = '1';
+  } else {
+    document.querySelector('.nav-menu').style.opacity = '0';
+  }
 });
